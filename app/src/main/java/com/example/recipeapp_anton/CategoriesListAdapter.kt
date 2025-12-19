2b07effba5935e0810148f57f92cf429129e30ba
Anton <new_perspective@mail.ru>
2025-12-19 12:59:56 +0300

    RA-08 adapter and layouts corrections

--- app/src/main/java/com/example/recipeapp_anton/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipeapp_anton/CategoriesListAdapter.kt
@@ -3,26 +3,26 @@ package com.example.recipeapp_anton
 import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recipeapp_anton.databinding.ItemCategoryBinding
 import models.Category
 
 class CategoriesListAdapter(val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.ivCategoryCard)
-        val titleTextView: TextView = view.findViewById(R.id.tvTitleCard)
-        val descriptionTextView: TextView = view.findViewById(R.id.tvDescriptionCard)
+    class ViewHolder(binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+        val imageView: ImageView = binding.ivCategoryCard
+        val titleTextView: TextView = binding.tvTitleCard
+        val descriptionTextView: TextView = binding.tvDescriptionCard
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
-        val view = inflater.inflate(R.layout.item_category, viewGroup, false)
-        return ViewHolder(view)
+        val binding = ItemCategoryBinding.inflate(inflater, viewGroup, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
@@ -31,17 +31,16 @@ class CategoriesListAdapter(val dataSet: List<Category>) :
         viewHolder.titleTextView.text = category.title
         viewHolder.descriptionTextView.text = category.description
 
-        val drawable =
-            try {
-                Drawable.createFromStream(
-                    viewHolder.itemView.context.applicationContext.assets.open(
-                        category.imageUrl
-                    ), null
-                )
-            } catch (e: Exception) {
-                Log.i("catch exception", "Image not found: ${category.imageUrl}")
-                null
-            }
+        val drawable = try {
+            viewHolder.itemView.context.applicationContext.assets
+                .open(category.imageUrl)
+                .use { inputStream ->
+                    Drawable.createFromStream(inputStream, null)
+                }
+        } catch (e: Exception) {
+            Log.i("catch exception", "Image not found: ${category.imageUrl}")
+            null
+        }
         viewHolder.imageView.setImageDrawable(drawable)
     }
 
